791340e17642577600776ab1836cad92045069b9
otaria-o <roberta.garavaglia.it@gmail.com>
2023-01-15 20:47:35 +0100

    Add style FindPeople

--- client/src/components/findpeople/findpeople.jsx
+++ client/src/components/findpeople/findpeople.jsx
@@ -10,15 +10,43 @@ export function FindPeople ({ first, finds, changeFindUsers }) {
         fetch("/users")
         .then(res => res.json())
         .then(newFinds => {
-            changeFindUsers(newFinds)
+            // changeFindUsers(newFinds)
             setFind(newFinds)
             console.log("dati arrivati al component", newFinds)
         })
     }, [])
 
-    const handleFinds = function(evt) {
-        console.log(evt)
-        setSearch(evt)
+    // const handleFinds = function(evt) {
+    //     console.log(evt)
+    //     setSearch(evt)
+    //     fetch("/users", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+                
+    //             newFinds: setFind(newFinds)
+    //             // errorMessage: this.state.errorMessage
+    //         })
+    //     })
+    //     .then(res => {
+    //         return res.json();
+    //     })
+    //     .then(data => {
+    //         console.log("dati dalla post del FindPeople component", data); 
+    //         // this.props.changeFindUsers()
+    //         setFind(data)
+    //     })
+    //     .catch(err => {
+    //         console.log("errore nella fetch!!", err)
+    //         // this.setState({ errorMessage: "Sorry, something went wrong. Fill up all the fields, please." })
+    //     })
+    // }
+
+    // const handleFinds = function(evt) {
+    //     console.log(evt)
+    //     setSearch(evt)
         // fetch("/users", {
         //     method: "POST",
         //     headers: {
@@ -42,10 +70,7 @@ export function FindPeople ({ first, finds, changeFindUsers }) {
         //     console.log("errore nella fetch!!", err)
         //     // this.setState({ errorMessage: "Sorry, something went wrong. Fill up all the fields, please." })
         // })
-    }
-
-
-    
+    // }
 
     return ( <div>
             <h2>Find people</h2>
@@ -59,7 +84,7 @@ export function FindPeople ({ first, finds, changeFindUsers }) {
                         <img src={newFind.image} />
                         </div>
                         <div>
-                        <h3>{newFind.first}{newFind.last}</h3>
+                        <h3>{newFind.first} {newFind.last}</h3>
                         </div>
                     </div>))}
                 </div>
